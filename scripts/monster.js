cc86ee07a6fd8f428d6884af7b3c05dd382841ad
sharfuddinzishan <sharfuddin.ahamed91@gmail.com>
2021-09-07 02:21:35 +0600

    [Added] No Data Found Handling

--- scripts/monster.js
+++ scripts/monster.js
@@ -4,27 +4,35 @@ const inputMonster = document.getElementById('inputMonster');
 const searchButton = document.getElementById('searchButton');
 const monsters = document.getElementById('monsters');
 const errorMsg = document.getElementById('errorMsg');
-
+let dataFound;
 searchButton.addEventListener('click', showMonsters);
 inputMonster.addEventListener('keyup', showMonsters);
 
 function showMonsters() {
     let searchText = inputMonster.value.toLowerCase();
+    dataFound = 'No'
     if (!searchText) {
+        monsters.textContent = '';
         return
     }
     inputMonster.textContent = '';
     monsters.textContent = '';
+    errorMsg.textContent = '';
     monsterdb.forEach(monster => {
         getMonster(monster, searchText);
     });
+    if (dataFound === 'No') {
+        errorNoDataFound();
+    }
 }
 
 let getMonster = (singleMonster, searchText) => {
     let { id, name, username, email } = singleMonster;
     if (name.toLowerCase().includes(searchText) || email.toLowerCase().includes(searchText)) {
+        dataFound = 'Yes';
         displayMonster(id, name, username, email);
     }
+    dataFound === 'Yes' ? '' : dataFound = 'No'
 }
 
 let displayMonster = (id, name, username, email) => {
@@ -41,4 +49,8 @@ let displayMonster = (id, name, username, email) => {
         </div>
     `;
     monsters.appendChild(monsterDiv);
+}
+
+let errorNoDataFound = () => {
+    errorMsg.innerHTML = "<h1 class='text-warning text-center fw-bold'>No Data Found</h1>";
 }
